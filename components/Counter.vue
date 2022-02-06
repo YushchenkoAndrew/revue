72f800897dd0aefd00146b8d118f7663a8ceda03
Andrew Y <Andrew10122000@gmail.com>
2022-02-06 02:58:01 +0200

    First review of vuejs

---
+++ components/Counter.vue
@@ -0,0 +1,21 @@
+<template>
+  <div v-if="c">
+    <p>Counter: {{ c }}</p>
+    <a @click="c--">Click Me!!</a>
+  </div>
+  <div v-else>Hello world</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: "Counter",
+  props: {
+    count: Number,
+  },
+  data() {
+    return { c: this.$props.count ?? 0 }
+  }
+})
+</script>
+
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Counter :count="9" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Counter from '../components/Counter.vue'
+
+export default defineComponent({
+  setup() {
+  },
+  components: { Counter }
+})
+</script>
